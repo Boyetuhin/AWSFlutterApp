567c803be4ad6fb5a4f32032afdfe937b2992d01
Tuhin Roy <36539534+Boyetuhin@users.noreply.github.com>
2020-11-09 23:28:06 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:awps/thirdPage.dart';
 import 'package:flutter/material.dart';
-// import 'package:oridsha_ngo/screens/JasonFile.dart';
-// import 'package:oridsha_ngo/screens/deviceDetails.dart';
-// import 'package:oridsha_ngo/screens/login_page.dart';
-// import 'package:oridsha_ngo/secondPage.dart';
-
 import 'SecondPage.dart';
 import 'first_page_login.dart';
-// import 'package:camera/camera.dart';
-// import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -21,9 +14,6 @@ class MyApp extends StatelessWidget {
         routes: <String, WidgetBuilder>{
           "/SecondPage": (BuildContext context) => new SecondPage(),
           "/ThirdPage": (BuildContext context) => new ThirdPage(),
-          // "/DeviceDetails": (BuildContext context) => new DeviceDetails(),
-          // "/RemoteJson" : (BuildContext context) => new RemoteJson(),
-          // "/CameraAccess":(BuildContext context) => new CameraAccess(),
         });
   }
 }
